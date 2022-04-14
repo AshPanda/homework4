727f40b9a422ba3d86ceefc0e95e80076207a8fc
AshPanda <alena.shwaika@gmail.com>
2022-04-14 19:45:09 +0300

    commit. Homework 4 minus Task 2

--- homework4/homework4.cpp
+++ homework4/homework4.cpp
@@ -15,10 +15,210 @@
 */
 
 #include <iostream>
+#include <vector>
+#include <cassert>
+//Task#1
+//Container from lesson 4
+class Container {
+	int* data;
+	int size;
+	int capacity;
+	int& operator[](int idx) {}
+
+public:
+	Container() : size(0), data(nullptr), capacity(0) { }
+	Container(int _size) : size(_size) {
+		if (_size == 0) {
+			Container();
+		}
+		else if (_size < 0) {
+			std::cout << "Bad thing happened" << std::endl;
+			Container();
+		}
+		else {
+			this->data = new int[_size];
+			this->capacity = 0;
+		}
+	}
+	virtual ~Container() {
+		delete[] data;
+	}
+	void setData(int idx, int value) {
+		assert(idx >= 0 && idx < size);
+		this->data[idx] = value;
+	}
+	int getData(int idx) {
+		assert(idx >= 0 && idx < size);
+
+		return this->data[idx];
+	}
+	int capacity() {
+		return this->capacity;
+	}
+	void resize(int newLength) {
+
+		if (newLength == size) return;
+		if (newLength <= 0) {
+
+			return;
+		}
+		int* newData = new int[newLength];
+		if (size > 0) {
+			int elemToCopy = (newLength > size) ? size : newLength;
+			for (int idx = 0; idx < elemToCopy; idx++) {
+
+				newData[idx] = data[idx];
+			}
+		}
+		delete[] data;
+		data = newData;
+		size = newLength;
+	}
+	void erase() {
+		capacity = 0;
+		for (int idx = 0; idx < size; idx++) {
+			data[idx] = 0;
+		}
+	}
+// homework
+	int removeLast() {
+		int temp = getData(size - 1);
+		resize(size - 1);
+		return temp;
+	}
+	int removeFirst() {
+		int temp = getData(0);
+		for (int i = 1; i < size; ++i)
+			data[i - 1] = data[i];
+
+		resize(size - 1);
+		return temp;
+	}
+	void sortArray() {
+		int temp;
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = (size - 1); j > i; j--)
+			{
+				if (data[j - 1] > data[j])
+				{temp = data[j - 1]; 
+					data[j - 1] = data[j]; 
+					data[j] = temp; }
+			}
+	}
+	void printArray() {
+		for (int i = 0; i < size; ++i)
+			std::cout << data[i] << " " << std::endl;
+	}
+}
+
+//Task#2 -
+//Task#3
+
+enum rank {
+	ACE = 1,
+	TWO = 2,
+	THREE = 3,
+	FOUR = 4,
+	FIVE = 5,
+	SIX = 6,
+	SEVEN = 7,
+	EIGHT = 8,
+	NINE = 9,
+	TEN = 10,
+	JACK = 10,
+	QUEEN = 10,
+	KING = 10
+};
+enum suit
+{
+	CLUBS,
+	DIAMONDS,
+	HEARTS,
+	SPADES
+};
+class Card
+{
+public:
+
+	Card(rank Rank, suit Suit) : Rank(Rank), Suit(Suit) {
+		isFaceUp = true;
+	}
+	~Card() {}
+	int GetValue() const {
+		return Rank;
+	}
+	void Flip() {
+		isFaceUp = !(isFaceUp);
+	}
+
+protected:
+	rank Rank;
+	suit Suit;
+	bool isFaceUp;
+};
+class Hand
+{
+public:
+	Hand();
+	virtual ~Hand();
+	void Add(Card* pCard) 
+	{
+		myCards.push_back(pCard);
+	}
+	void Clear() {
+		std::vector<Card*>::iterator i = myCards.begin();
+		for (i = myCards.begin(); i != myCards.begin(); ++i)
+		{
+			delete* i;
+			*i = 0;
+		}
+		myCards.clear();
+	}
+	int GetValue() const {
+		if (myCards.empty())
+		{
+			return 0;
+		}
+		if (myCards[0]->GetValue() == 0)
+		{
+			return 0;
+		}
+		int total = 0;
+		std::vector<Card*>::const_iterator i;
+		for (i = myCards.begin(); i != myCards.end(); ++i)
+		{
+			total += (*i)->GetValue();
+		}
+		bool isAce = false;
+		for (i = myCards.begin(); i != myCards.end(); ++i)
+		{
+			if ((*i)->GetValue() == rank::ACE)
+			{
+				isAce = true;
+			}
+		}
+		if (isAce && total <= 11)
+		{
+			total += 10;
+		}
+		return total;
+	}
+protected:
+	std::vector<Card*> myCards;
+};
+
+Hand::Hand()
+{
+	myCards.reserve(5);
+}
+
+Hand::~Hand()
+{
+	Clear();
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	return 0;
 }
-
-
